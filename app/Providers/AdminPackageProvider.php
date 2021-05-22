493e2b999e14fed8735fa9df45d04d84288e732a
easymagic <nnamware@yahoo.com>
2021-05-22 11:18:06 +0100

    np

--- app/Providers/AdminPackageProvider.php
+++ app/Providers/AdminPackageProvider.php
@@ -35,12 +35,14 @@ class AdminPackageProvider extends ServiceProvider
            __DIR__ . '/../../resources/views/auth'=>base_path('resources/views/auth'),
 
            __DIR__ . '/../../resources/views/admin-user'=>base_path('resources/views/admin-user'),
+           __DIR__ . '/../../resources/views/company'=>base_path('resources/views/company'),
+
            __DIR__ . '/../../app/Models/User.php'=>base_path('app/Models/User.php'),
            __DIR__ . '/../../app/Models/Company.php'=>base_path('app/Models/Company.php'),
 
            __DIR__ . '/../../app/Traits'=>base_path('app/Traits'),
 
-//           __DIR__ . '/../../database/factories/UserFactory.php'=>base_path('database/factories/UserFactory.php'),
+           __DIR__ . '/../../database/factories/UserFactory.php'=>base_path('database/factories/UserFactory.php'),
 //           __DIR__ . '/../../database/factories/CompanyFactory.php'=>base_path('database/factories/CompanyFactory.php'),
 
 
@@ -53,8 +55,9 @@ class AdminPackageProvider extends ServiceProvider
 
            //Migrations
            __DIR__ . '/../../database/migrations/2014_10_12_000000_create_users_table.php'=>base_path('database/migrations/2014_10_12_000000_create_users_table.php'),
-//           __DIR__ . '/../../database/migrations/2021_05_01_100945_create_companies_table.php'=>base_path('database/migrations/2021_05_01_100945_create_companies_table.php'),
+           __DIR__ . '/../../database/migrations/2021_05_22_085835_create_companies_table.php'=>base_path('database/migrations/2021_05_22_085835_create_companies_table.php'),
 
+           __DIR__ . '/../../app/Services'=>base_path('app/Services/')
 
 
 
