7f28b099a58eb5c70f2ef3426afbfe6d86ad72fb
easymagic <nnamware@yahoo.com>
2021-05-22 10:48:15 +0100

    np

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,83 +3,62 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use App\Services\CompanyService;
+use App\Traits\ResponseTraitV2;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use ResponseTraitV2;
+
+    private $data = [];
+
+    function loadCompany(){
+      $this->data['companies'] = CompanyService::fetch()->get();
+    }
+
     public function index()
     {
-        //
+        $this->loadCompany();
+        return view('company.index',$this->data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        return $this->resolveResponse(CompanyService::store());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Company $company)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Company $company)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Company $company)
+
+    public function update(Request $request, $id)
     {
         //
+        return $this->resolveResponse(CompanyService::update($id));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Company $company)
+
+    public function destroy($id)
     {
         //
+        return $this->resolveResponse(CompanyService::delete($id));
     }
+    
 }
